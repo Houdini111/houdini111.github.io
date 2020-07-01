d333ad72fbebd2fd404b8f629149622f90033451
Houdini111 <xela.notnef@gmail.com>
2020-07-01 13:10:02 -0600

    Piece rendering Stage 1

--- tetris_incremental/tetris_incremental.js
+++ tetris_incremental/tetris_incremental.js
@@ -1,36 +1,89 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var BOARD_WIDTH = 10;
 var BOARD_HEIGHT = 20;
-var DRAW_SCALE = 40;
-var GRID_LINE_WIDTH = 4;
+var DRAW_SCALE = 35;
+var GRID_LINE_WIDTH = 2;
 var grid_ctx;
 var board_ctx;
-var RenderableEntity = function () {
-    var x = 0;
-    var y = 0;
-    var image = null;
-    this.render = function (context) {
-        context.drawImage(this.image, this.x, this.y);
-    };
-};
-function render(context) {
-    //context.clearRect(0, 0, width, height);
-    //background.render(context);
+var Segment = /** @class */ (function () {
+    function Segment() {
+    }
+    return Segment;
+}());
+var Piece = /** @class */ (function () {
+    function Piece(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.x = x;
+        this.y = y;
+        this.segments = new Array();
+        for (var i = 0; i < 4; i++) {
+            this.segments.push(new Segment());
+        }
+    }
+    return Piece;
+}());
+var O_Piece = /** @class */ (function (_super) {
+    __extends(O_Piece, _super);
+    function O_Piece(x, y) {
+        var _this = _super.call(this, x, y) || this;
+        _this.color = "#FFFF00";
+        return _this;
+    }
+    return O_Piece;
+}(Piece));
+var start_time;
+var last_render_time;
+var current_piece;
+var delta_time;
+function render_dynamic_board() {
+    var now = Date.now();
+    delta_time = now - last_render_time;
+    last_render_time = now;
+    //let out: string = "RENDER. dt: " + delta_time;
+    if (current_piece) {
+        //out += " " + 2 * DRAW_SCALE;
+        board_ctx.fillStyle = current_piece.color;
+        board_ctx.fillRect(current_piece.x * DRAW_SCALE, current_piece.y * DRAW_SCALE, 2 * DRAW_SCALE, 2 * DRAW_SCALE);
+    }
+    //console.log(out);
 }
 function draw_grid() {
     grid_ctx.strokeStyle = "#000000";
     grid_ctx.lineWidth = GRID_LINE_WIDTH;
     //grid_ctx.globalAlpha = 1;
-    for (var x = GRID_LINE_WIDTH / 2 | 0; x < (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH; x += DRAW_SCALE) {
+    var w = grid_ctx.canvas.width;
+    var h = grid_ctx.canvas.height;
+    grid_ctx.clearRect(0, 0, grid_ctx.canvas.width, grid_ctx.canvas.height);
+    for (var x = (GRID_LINE_WIDTH / 2 | 0); x < w; x += DRAW_SCALE) {
         grid_ctx.moveTo(x, 0);
-        grid_ctx.lineTo(x, (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH);
+        grid_ctx.lineTo(x, h);
     }
-    for (var y = GRID_LINE_WIDTH / 2 | 0; y < (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH; y += DRAW_SCALE) {
+    for (var y = (GRID_LINE_WIDTH / 2 | 0); y < h; y += DRAW_SCALE) {
         grid_ctx.moveTo(0, y);
-        grid_ctx.lineTo((BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH, y);
+        grid_ctx.lineTo(w, y);
     }
     grid_ctx.stroke();
 }
 function on_load() {
+    prepare_canvases();
+    draw_grid();
+    start_keypress_listener();
+    start_render_thread();
+}
+function prepare_canvases() {
     var oldMoveTo = CanvasRenderingContext2D.prototype.moveTo; //For sharpening, see: https://stackoverflow.com/a/23613785/4698411
     CanvasRenderingContext2D.prototype.moveTo = function (x, y) {
         x |= 0;
@@ -43,22 +96,37 @@ function on_load() {
         y |= 0;
         oldLineTo.call(this, x, y);
     };
-    var even_offset = GRID_LINE_WIDTH % 2 ? 0 : 1;
+    var odd_offset = GRID_LINE_WIDTH % 2 ? 1 : 0;
     var grid_obj = document.getElementById("board_grid");
     grid_ctx = grid_obj.getContext("2d");
-    grid_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    grid_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    if (GRID_LINE_WIDTH % 2 !== 0) {
+    grid_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH;
+    grid_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH;
+    if (odd_offset) {
         grid_ctx.translate(0.5, 0.5); //For sharpening, see: https://stackoverflow.com/a/23613785/4698411
     }
     var board_obj = document.getElementById("board");
     board_ctx = board_obj.getContext("2d");
-    board_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    board_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    if (GRID_LINE_WIDTH % 2 !== 0) {
+    board_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH;
+    board_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH;
+    if (odd_offset) {
         board_ctx.translate(0.5, 0.5);
     }
-    draw_grid();
+}
+function start_keypress_listener() {
+    document.addEventListener("keydown", function (event) {
+        if (event.shiftKey) {
+        }
+        else if (event.keyCode === 32) { //Space
+            if (!current_piece) {
+                current_piece = new O_Piece(8, 18);
+            }
+        }
+    });
+}
+function start_render_thread() {
+    start_time = Date.now();
+    last_render_time = start_time;
+    setInterval(render_dynamic_board, 16.66);
 }
 window.onload = on_load;
 //# sourceMappingURL=tetris_incremental.js.map
--- tetris_incremental/tetris_incremental.ts
+++ tetris_incremental/tetris_incremental.ts
@@ -1,24 +1,58 @@
 const BOARD_WIDTH: number = 10;
 const BOARD_HEIGHT: number = 20; 
-const DRAW_SCALE: number = 40;
-const GRID_LINE_WIDTH: number = 4;
+const DRAW_SCALE: number = 35;
+const GRID_LINE_WIDTH: number = 2;
 
 let grid_ctx: CanvasRenderingContext2D;
 let board_ctx: CanvasRenderingContext2D;
 
-let RenderableEntity = function () {
-    let x: number = 0;
-    let y: number = 0;
-    let image: object = null;
+class Segment {
 
-    this.render = function (context: any) {
-        context.drawImage(this.image, this.x, this.y);
+}
+
+class Piece {
+    x: number = 0;
+    y: number = 0;
+    segments: Segment[];
+    color: string;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.segments = new Array();
+        for (let i: number = 0; i < 4; i++) {
+            this.segments.push(new Segment());
+        }
     }
 }
 
-function render(context: any): void {
-    //context.clearRect(0, 0, width, height);
-    //background.render(context);
+class O_Piece extends Piece {
+    constructor(x: number, y: number) {
+        super(x, y);
+        this.color = "#FFFF00";
+    }
+}
+
+let start_time: number;
+let last_render_time: number;
+let current_piece: Piece;
+let delta_time: number;
+
+
+function render_dynamic_board(): void {
+    let now = Date.now();
+    delta_time = now - last_render_time;
+    last_render_time = now;
+
+    //let out: string = "RENDER. dt: " + delta_time;
+
+    if (current_piece) {
+        //out += " " + 2 * DRAW_SCALE;
+        board_ctx.fillStyle = current_piece.color;
+        board_ctx.fillRect(current_piece.x*DRAW_SCALE, current_piece.y*DRAW_SCALE, 2 * DRAW_SCALE, 2 * DRAW_SCALE);
+    }
+
+    //console.log(out);
 }
 
 function draw_grid(): void {
@@ -26,18 +60,33 @@ function draw_grid(): void {
     grid_ctx.lineWidth = GRID_LINE_WIDTH;
     //grid_ctx.globalAlpha = 1;
 
-    for (let x: number = GRID_LINE_WIDTH / 2 | 0; x < (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH; x += DRAW_SCALE) {
+    let w: number = grid_ctx.canvas.width;
+    let h: number = grid_ctx.canvas.height;
+
+    grid_ctx.clearRect(0, 0, grid_ctx.canvas.width, grid_ctx.canvas.height);
+
+    for (let x: number = (GRID_LINE_WIDTH/2|0); x < w; x += DRAW_SCALE) {
         grid_ctx.moveTo(x, 0);
-        grid_ctx.lineTo(x, (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH);
+        grid_ctx.lineTo(x, h);
     }
-    for (let y: number = GRID_LINE_WIDTH/2|0; y < (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH; y += DRAW_SCALE) {
+    for (let y: number = (GRID_LINE_WIDTH/2|0); y < h; y += DRAW_SCALE) {
         grid_ctx.moveTo(0, y);
-        grid_ctx.lineTo((BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH, y);
+        grid_ctx.lineTo(w, y);
     }
     grid_ctx.stroke();
 }
 
 function on_load(): void {
+    prepare_canvases();
+
+    draw_grid();
+
+    start_keypress_listener();
+    
+    start_render_thread();
+}
+
+function prepare_canvases(): void {
     let oldMoveTo = CanvasRenderingContext2D.prototype.moveTo; //For sharpening, see: https://stackoverflow.com/a/23613785/4698411
     CanvasRenderingContext2D.prototype.moveTo = function (x, y) {
         x |= 0;
@@ -51,27 +100,42 @@ function on_load(): void {
         oldLineTo.call(this, x, y);
     }
 
-    let even_offset = GRID_LINE_WIDTH % 2 ? 0 : 1;
+    let odd_offset :number = GRID_LINE_WIDTH % 2 ? 1 : 0;
 
     let grid_obj: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("board_grid");
     grid_ctx = grid_obj.getContext("2d");
-    grid_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    grid_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    if (GRID_LINE_WIDTH % 2 !== 0) { 
+    grid_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH;
+    grid_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH;
+    if (odd_offset) {
         grid_ctx.translate(0.5, 0.5); //For sharpening, see: https://stackoverflow.com/a/23613785/4698411
     }
 
     let board_obj: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("board");
     board_ctx = board_obj.getContext("2d");
-    board_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    board_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH + even_offset;
-    if (GRID_LINE_WIDTH % 2 !== 0) {
+    board_ctx.canvas.width = (BOARD_WIDTH * DRAW_SCALE) + GRID_LINE_WIDTH;
+    board_ctx.canvas.height = (BOARD_HEIGHT * DRAW_SCALE) + GRID_LINE_WIDTH;
+    if (odd_offset) {
         board_ctx.translate(0.5, 0.5);
     }
+}
 
-    draw_grid();
+function start_keypress_listener() :void {
+    document.addEventListener("keydown", function (event) {
+        if (event.shiftKey) {
 
+        }
+        else if (event.keyCode === 32) { //Space
+            if (!current_piece) {
+                current_piece = new O_Piece(8, 18);
+            }
+        }
+    });
+}
 
+function start_render_thread(): void {
+    start_time = Date.now();
+    last_render_time = start_time;
+    setInterval(render_dynamic_board, 16.66);
 }
 
 window.onload = on_load;
